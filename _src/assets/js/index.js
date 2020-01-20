19f90e130c45cdaf12e3101593b2caae50361429
Maria Garcia de Lomana <mariagarciadelomana@gmail.com>
2020-01-20 22:01:31 +0100

    create index.js and begin distributing variables amongst the js components

---
+++ _src/assets/js/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const constants = require('./00-constants');
+const dropDownMenu = require('./01-dropDownMenu');
+const palette = require('./02-palette');
+const userData = require('./03-userData');
+const getAvatar = require('./04-get-avatar');
+const buttonReset = require('./05-button-reset');
+const fetch = require('./06-fetch');
