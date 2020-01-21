67e66ab4f31957a94242c7a8719adca538e8cb42
Maria Garcia de Lomana <mariagarciadelomana@gmail.com>
2020-01-21 19:56:01 +0100

    review dropDownMenu js component

--- _src/assets/js/components/dropDownMenu.js
+++ _src/assets/js/components/dropDownMenu.js
@@ -1,44 +1,53 @@
-'use strict';
+"use strict";
 
-const fetch = require('../services/fetch');
+const constants = require("./constants");
+const createCard = require("./createCard");
 
+const designContainer = document.querySelector(constants.designContainer);
+const fillInContainer = document.querySelector(constants.fillInContainer);
+const shareContainer = document.querySelector(constants.shareContainer);
+const arrowDesign = document.querySelector(constants.arrowDesign);
+const arrowFillIn = document.querySelector(constants.arrowFillIn);
+const arrowShare = document.querySelector(constants.arrowShare);
+const design = document.querySelector(constants.design);
+const fillIn = document.querySelector(constants.fillIn);
+const share = document.querySelector(constants.share);
+const defaultImage = constants.defaultImage;
 
 // start page functions
 
-arrowDesign.classList.add('rotate');
-fillIn.classList.add('hidden');
-share.classList.add('hidden');
+arrowDesign.classList.add("rotate");
+fillIn.classList.add("hidden");
+share.classList.add("hidden");
 
 function hideDesign() {
-  design.classList.toggle('hidden');
-  fillIn.classList.add('hidden');
-  share.classList.add('hidden');
-  arrowDesign.classList.toggle('rotate');
-  arrowFillIn.classList.remove('rotate');
-  arrowShare.classList.remove('rotate');
-
+  design.classList.toggle("hidden");
+  fillIn.classList.add("hidden");
+  share.classList.add("hidden");
+  arrowDesign.classList.toggle("rotate");
+  arrowFillIn.classList.remove("rotate");
+  arrowShare.classList.remove("rotate");
 }
-designContainer.addEventListener('click', hideDesign);
+designContainer.addEventListener("click", hideDesign);
 
 function toggleFillIn() {
-  fillIn.classList.toggle('hidden');
-  design.classList.add('hidden');
-  share.classList.add('hidden');
-  arrowDesign.classList.remove('rotate');
-  arrowFillIn.classList.toggle('rotate');
-  arrowShare.classList.remove('rotate');
-
+  fillIn.classList.toggle("hidden");
+  design.classList.add("hidden");
+  share.classList.add("hidden");
+  arrowDesign.classList.remove("rotate");
+  arrowFillIn.classList.toggle("rotate");
+  arrowShare.classList.remove("rotate");
 }
-fillInContainer.addEventListener('click', toggleFillIn);
+fillInContainer.addEventListener("click", toggleFillIn);
 
 function toggleShare() {
-  share.classList.toggle('hidden');
-  design.classList.add('hidden');
-  fillIn.classList.add('hidden');
-  arrowShare.classList.toggle('rotate');
-  arrowDesign.classList.remove('rotate');
-  arrowFillIn.classList.remove('rotate');
+  share.classList.toggle("hidden");
+  design.classList.add("hidden");
+  fillIn.classList.add("hidden");
+  arrowShare.classList.toggle("rotate");
+  arrowDesign.classList.remove("rotate");
+  arrowFillIn.classList.remove("rotate");
 }
-shareContainer.addEventListener('click', toggleShare);
-shareContainer.addEventListener('click', fetch.checkFilledInputs)
-twitterContainer.classList.add('hidden');
+shareContainer.addEventListener("click", toggleShare);
+shareContainer.addEventListener("click", createCard.checkFilledInputs);
+twitterContainer.classList.add("hidden");
--- _src/assets/js/index.js
+++ _src/assets/js/index.js
@@ -1,9 +1,13 @@
-'use strict';
+"use strict";
 
-const constants = require('./components/constants');
-const dropDownMenu = require('./components/dropDownMenu');
-const palette = require('./components/palettes');
-const userData = require('./components/userData');
-const avatar = require('./components/avatar');
-const buttonReset = require('./components/reset');
-const fetch = require('./services/fetch');
+const constants = require("./components/constants");
+const dropDownMenu = require("./components/dropDownMenu");
+const palettes = require("./components/palettes");
+const userData = require("./components/userData");
+const reset = require("./components/reset");
+const form = require("./components/form");
+// const validation = require("./components/validation");
+const fonts = require("./components/fonts");
+const createCard = require("./components/createCard");
+const avatar = require("./components/avatar");
+const api = require("./services/api");
